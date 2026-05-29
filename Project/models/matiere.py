96e3c1a9e13b977d62161876d8a129e73175e70a
PARE kowoma Imane Zilal <imanepare7@gmail.com>
2026-05-29 10:50:26 +0000

    matiere.py

--- Project/models/matiere.py
+++ Project/models/matiere.py
@@ -13,19 +13,11 @@ class Matiere:
 
     def __init__(self, code: str, nom: str, credits: int,
                  professeur_id: str = ""):
-        """
-        Constructeur de Matiere.
-        :param code: code unique (ex: MATH101)
-        :param nom: nom complet de la matière
-        :param credits: nombre de crédits (int)
-        :param professeur_id: ID du professeur assigné
-        """
+        "Constructeur de Matiere."
         self.__code          = code
         self.__nom           = nom
-        self.__credits: int  = credits      # type int
+        self.__credits: int  = credits      
         self.__professeur_id = professeur_id
-
-    # ── Getters ──────────────────────────────────────────────
 
     def get_code(self) -> str:
         """Retourne le code de la matière."""
@@ -43,13 +35,9 @@ class Matiere:
         """Retourne l'ID du professeur assigné."""
         return self.__professeur_id
 
-    # ── Setter ───────────────────────────────────────────────
-
     def set_professeur(self, prof_id: str):
         """Assigne un professeur à cette matière."""
         self.__professeur_id = prof_id
-
-    # ── Affichage ────────────────────────────────────────────
 
     def afficher_info(self):
         """Affiche les informations de la matière."""
@@ -57,7 +45,6 @@ class Matiere:
         print(f"  [{self.__code}] {self.__nom} — "
               f"{self.__credits} crédit(s) — Prof ID: {prof}")
 
-    # ── Sérialisation JSON ────────────────────────────────────
 
     def to_dict(self) -> dict:
         """Convertit la matière en dictionnaire pour sauvegarde JSON."""
