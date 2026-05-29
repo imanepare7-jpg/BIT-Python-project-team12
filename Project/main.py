c9b6d9fdbff8718b3fc010b3bf924cc7459ef88a
PARE kowoma Imane Zilal <imanepare7@gmail.com>
2026-05-29 10:53:06 +0000

    main.py

--- Project/main.py
+++ Project/main.py
@@ -1,18 +1,12 @@
-# ── Imports ──────────────────────────────────────────────────
-from school      import School
-from menus      import main_menu
-from constants  import SCHOOL_NAME
-from models     import Student, Teacher  
-
-
-# ── Main Function ───────────────────────────────────────
+from school import School
+from menus import main_menu
+from constants import SCHOOL_NAME
+from models import Student, Teacher  
 
 def main():
     school = School(SCHOOL_NAME)
     school.load()          
     main_menu(school) 
-
-
-# ── Execution ─────────────────────────────────────────────────
+    
 if __name__ == "__main__":
     main()
