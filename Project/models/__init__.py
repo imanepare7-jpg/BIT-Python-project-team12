e10d524651e836208a517d7f1aa5b448f395c9ec
PARE kowoma Imane Zilal <imanepare7@gmail.com>
2026-05-30 19:39:42 +0000

    Update __init__.py

--- Project/models/__init__.py
+++ Project/models/__init__.py
@@ -1,10 +1,3 @@
-"""
-models/__init__.py
-------------------
-Initializes the models package.
-Allows easy importing of all classes from outside.
-"""
-
 from models.person  import Person
 from models.student import Student
 from models.teacher import Teacher
