266c90d222299d2070e240dc68fe544f0c874b9e
Sagnon Aminata <aminatasagnon02@gmail.com>
2026-05-30 06:44:55 -0700

    Enhance Teacher class with specialization and phone
    Added specialization and phone number attributes to Teacher class.

--- Project/models/teacher.py
+++ Project/models/teacher.py
@@ -5,11 +5,22 @@ Contains the Teacher class which inherits from Person.
 """
 
 from models.person import Person
-class Teacher(Person):
 
-  
+
+class Teacher(Person):
     """
     Represents a teacher. Inherits from Person.
     Adds: specialization, phone number, list of subjects taught.
     Demonstrates: inheritance, polymorphism.
-    
+    """
+def __init__(self, id: str, last_name: str, first_name: str, email: str,
+                 specialization: str, phone: str):
+        """
+        Constructor for Teacher.
+        :param specialization: area of expertise
+        :param phone: phone number
+        """
+        super().__init__(id, last_name, first_name, email)  # parent constructor
+        self.__specialization = specialization
+        self.__phone          = phone
+        self.__subjects: list = []
