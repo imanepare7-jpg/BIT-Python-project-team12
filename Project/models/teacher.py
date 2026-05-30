210f872afdb5e0c873d2d886cbccfaab005f6957
Sagnon Aminata <aminatasagnon02@gmail.com>
2026-05-30 06:33:45 -0700

    Add Teacher class with specialization and subjects
    Introduced Teacher class inheriting from Person with additional attributes.

--- Project/models/teacher.py
+++ Project/models/teacher.py
@@ -5,3 +5,11 @@ Contains the Teacher class which inherits from Person.
 """
 
 from models.person import Person
+class Teacher(Person):
+
+  
+    """
+    Represents a teacher. Inherits from Person.
+    Adds: specialization, phone number, list of subjects taught.
+    Demonstrates: inheritance, polymorphism.
+    
